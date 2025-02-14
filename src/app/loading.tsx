168affc051f2d3ba79524baecd934f3805977c42
Ahmed Adel <ahmedadel10888@gmail.com>
2025-02-14 02:33:19 +0200

    edit loading styles

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <div className="w-16 h-16 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
+      <div className="w-14 h-14 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
