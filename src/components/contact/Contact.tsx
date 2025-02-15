b0f20be87ee8721896d57dac3afed4bb3f7a926d
Ahmed Adel <ahmedadel10888@gmail.com>
2025-02-15 02:01:19 +0200

    change loading width and height

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ContactForm from "./ContactForm";
 
 const Contact = () => {
@@ -14,15 +15,13 @@ const Contact = () => {
         <div className="flex flex-col gap-10 lg:flex-row mt-20 pb-10">
           <ContactForm />
 
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6910.394485712961!2d31.43184190113411!3d30.002508685325576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583d005d1f4387%3A0x3afe7c329b285ed3!2sIntegration%20insurance%20brokerage!5e0!3m2!1sar!2seg!4v1739571601147!5m2!1sar!2seg"
-            width="600"
-            height="450"
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
+          <Image
+            src="/assets/home-imgs/hero.jpg"
+            alt="hero"
+            width={550}
+            height={450}
             className="border rounded-xl min-w-full max-h-[430px] lg:min-w-[450px] xl:min-w-[550px]"
-          ></iframe>
+          />
         </div>
       </div>
     </section>
