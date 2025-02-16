c6326d11548dd8c68e29351ce8b8d01659a4165d
Ahmed Adel <ahmedadel10888@gmail.com>
2025-02-16 14:35:46 +0200

    fix error in image slider section

--- src/components/imagSlider/ImagSlider.tsx
+++ src/components/imagSlider/ImagSlider.tsx
@@ -1,8 +1,6 @@
 "use client";
-
 import Image from "next/image";
 import Slider from "react-slick";
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { sliderInfo } from "@/constants/ImagSliderInfo";
