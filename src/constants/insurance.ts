96ddedbd0775c9807d38c276d19e4feb3f7ad823
Gigalord <dev.elbehery@gmail.com>
2025-02-17 20:59:56 +0200

    Add new images for insurances

--- src/constants/insurance.ts
+++ src/constants/insurance.ts
@@ -6,102 +6,102 @@ type Insurances = {
 
 export const insuranceCategories: Insurances[] = [
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (8).webp",
+    insuranceSrc: "/assets/insurance-imgs/1.jpg",
     insuranceTitle: "- تأمين الممتلكات.",
     insuranceDescription:
       "يغطى التأمين على الأصول والممتلكات مثل ( الحريق – السطو- تهدم المباني ) الخاصة بالانشطة الصناعية والتجارية.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (9).webp",
+    insuranceSrc: "/assets/insurance-imgs/2.jpg",
     insuranceTitle: "- تأمين الحوادث المتنوعة و المسئوليات.",
     insuranceDescription:
       "يشمل الحوادث الشخصية – خيانة الأمانة – نقل النقدية (السرقة بالاكراه)  والمسئوليات بأنواعها .",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (10).webp",
+    insuranceSrc: "/assets/insurance-imgs/3.jpg",
     insuranceTitle: "- التأمين الهندسي.",
     insuranceDescription:
       "يغطى الأضرار التي قد تحدث أثناء تنفيذ المشروعات الهندسية او التى تحدث بعدها بسبب خطا أثناء تنفيذ تلك المشروعات",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (11).webp",
+    insuranceSrc: "/assets/insurance-imgs/4.jpg",
     insuranceTitle: "- تأمين النقل البحري / الجوي / البري.",
     insuranceDescription:
       "يحمي الشحنات والبضائع أثناء نقلها عبر وسائل النقل المختلفة.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (12).webp",
+    insuranceSrc: "/assets/insurance-imgs/5.jpg",
     insuranceTitle: "- تأمين أجسام السفن.",
     insuranceDescription:
       "يوفر تغطية للأضرار التي قد تصيب السفن أثناء التشغيل أو الحوادث البحرية.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (13).webp",
+    insuranceSrc: "/assets/insurance-imgs/6.jpg",
     insuranceTitle: "- تأمين السيارات.",
     insuranceDescription:
       "يشمل الأضرار التي تصيب السيارات أو المسؤولية تجاه الغير في حالة الحوادث.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (1).webp",
+    insuranceSrc: "/assets/insurance-imgs/7.webp",
     insuranceTitle: "- تأمين مخاطر الائتمان.",
     insuranceDescription: "يحمي من مخاطر عدم سداد القروض و الديون المستحقة.",
   },
   // {
-  //   insuranceSrc: "/assets/insurance-imgs/insurance (2).webp",
+  //   insuranceSrc: "/assets/insurance-imgs/insurance (2).jpg",
   //   insuranceTitle: "- التأمين ضد الأخطار الخاصة.",
   //   insuranceDescription:
   //     "يغطي الحوادث أو الأضرار الخاصة التي لا تندرج تحت التأمينات التقليدية.",
   // },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (3).webp",
+    insuranceSrc: "/assets/insurance-imgs/8.jpg",
     insuranceTitle: "- تأمينات الأفراد / الشركات (حياة - طبي - معاش - تعليم)",
     insuranceDescription:
       "تشمل تأمين الحياة، الطبي، المعاش، والتعليم لحماية الأفراد ماليًا وصحيًا.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (4).webp",
+    insuranceSrc: "/assets/insurance-imgs/9.jpg",
     insuranceTitle: "- تأمين حماية المنزل.",
     insuranceDescription:
       "يعوض عن الأضرار التي قد تصيب المنزل بسبب الحرائق، السرقة، أو الكوارث الطبيعية.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (5).webp",
+    insuranceSrc: "/assets/insurance-imgs/10.jpg",
     insuranceTitle: "- تأمين بترول.",
     insuranceDescription:
       "يغطي المخاطر المتعلقة بصناعة النفط والغاز، مثل الحوادث والتسربات النفطية.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (6).webp",
+    insuranceSrc: "/assets/insurance-imgs/11.jpg",
     insuranceTitle: "- تأمين العنف السياسي و الارهاب.",
     insuranceDescription:
       "يحمي الأفراد والشركات من الخسائر الناتجة عن أعمال الشغب، التخريب، والهجمات الإرهابية.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (7).webp",
+    insuranceSrc: "/assets/insurance-imgs/12.jpg",
     insuranceTitle: "- امكانية التأمين داخل و خارج مصر.",
     insuranceDescription:
       "يتيح للأفراد والشركات الحصول على تغطية تأمينية محلية أو دولية حسب الحاجة.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (14).webp",
+    insuranceSrc: "/assets/insurance-imgs/13.jpg",
     insuranceTitle: "- تأمين المنتجات.",
     insuranceDescription:
       "يغطى الخطر الذى قد يصيب الافراد او الجهات من ضرر استخدام منتج معين.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (15).webp",
+    insuranceSrc: "/assets/insurance-imgs/14.jpg",
     insuranceTitle: "- تأمين القطع الثمينة.",
     insuranceDescription:
       "يوفر حماية شاملة للقطع الثمينة مثل المجوهرات، الساعات الفاخرة، والتحف، مع تغطية محلية ودولية.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (16).webp",
+    insuranceSrc: "/assets/insurance-imgs/15.jpg",
     insuranceTitle: "- تأمين حماية الموظفين والمديرين.",
     insuranceDescription:
       "يغطى التصرفات الناتجة بصفتهم بسبب اتخاذ قرارات خاطئة ينتج عنها ضرر للجهه التي يعملون بها.",
   },
   {
-    insuranceSrc: "/assets/insurance-imgs/insurance (17).webp",
+    insuranceSrc: "/assets/insurance-imgs/16.webp",
     insuranceTitle: "- تأمين ضد الهجمات السيبرانية والاختراقات الالكترونية.",
     insuranceDescription:
       "توفر الحماية من الاختراقات والهجمات الالكترونية التي ينتج عنها ضرر من سرقة البيانات والمعلومات مثل البنوك والمصانع والشركات.",
