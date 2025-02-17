3a9a1205713f016caa01d72a33c052a1169e42c4
Ahmed Adel <ahmedadel10888@gmail.com>
2025-02-17 17:58:20 +0200

    change images width in image slider

--- src/constants/ImagSliderInfo.ts
+++ src/constants/ImagSliderInfo.ts
@@ -5,31 +5,31 @@ type SliderInfo = {
 
 export const sliderInfo: SliderInfo[] = [
   {
-    sliderSrc: "/assets/trust/trust (2).jpg",
+    sliderSrc: "/assets/trust/trust (2).png",
     sliderAlt: "شركة المهندس للتأمين",
   },
   {
-    sliderSrc: "/assets/trust/trust (3).jpg",
+    sliderSrc: "/assets/trust/trust (3).png",
     sliderAlt: "AXA Egypt company",
   },
   {
-    sliderSrc: "/assets/trust/trust (5).jpg",
+    sliderSrc: "/assets/trust/trust (5).png",
     sliderAlt: "مصر للتأمينات",
   },
   {
-    sliderSrc: "/assets/trust/trust (6).jpg",
+    sliderSrc: "/assets/trust/trust (6).png",
     sliderAlt: "اورينت للتامين ",
   },
   {
-    sliderSrc: "/assets/trust/trust (7).jpg",
+    sliderSrc: "/assets/trust/trust (7).png",
     sliderAlt: "SARWA insurance company",
   },
   {
-    sliderSrc: "/assets/trust/trust (9).webp",
+    sliderSrc: "/assets/trust/trust (9).png",
     sliderAlt: "المصريه للتأمين التكافلي",
   },
   {
-    sliderSrc: "/assets/trust/trust (12).jpg",
+    sliderSrc: "/assets/trust/trust (12).png",
     sliderAlt: "SARWA LIFE company",
   },
   {
@@ -37,19 +37,19 @@ export const sliderInfo: SliderInfo[] = [
     sliderAlt: "Allianz company",
   },
   {
-    sliderSrc: "/assets/trust/trust (8).webp",
+    sliderSrc: "/assets/trust/trust (8).png",
     sliderAlt: "SALAMA insurance company",
   },
   {
-    sliderSrc: "/assets/trust/trust (4).webp",
+    sliderSrc: "/assets/trust/trust (4).png",
     sliderAlt: "mada insurance company",
   },
   {
-    sliderSrc: "/assets/trust/trust (11).webp",
+    sliderSrc: "/assets/trust/trust (11).png",
     sliderAlt: "ROYAL insurance company",
   },
   {
-    sliderSrc: "/assets/trust/trust (10).webp",
+    sliderSrc: "/assets/trust/trust (10).png",
     sliderAlt: "مصر للتأمين التكافلي",
   },
 ];
