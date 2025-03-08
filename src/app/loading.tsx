9b2a27f2a5345baf0b03e4aeeecafaab259d9a8f
Ahmedadel1414 <ahmedadel10888@gmail.com>
2025-03-08 22:57:31 +0200

    Done

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <div className="w-16 h-16 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
+      <div className="w-14 h-14 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
